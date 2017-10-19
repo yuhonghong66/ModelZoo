ae6d4b06b6f335de08c2b44e5a1ff0077e39b7c7
Tomasz Patejko <tomasz.patejko@intel.com>
2017-10-20 00:39:06 +0200

    VGG changed for new Neon

--- ImageClassification/ILSVRC2012/VGG/vgg_neon.py
+++ ImageClassification/ILSVRC2012/VGG/vgg_neon.py
@@ -20,7 +20,7 @@ Based on manuscript:
     K. Simonyan, A. Zisserman
     arXiv:1409.1556
 """
-
+import numpy as np
 from neon.util.argparser import NeonArgparser
 from neon.backends import gen_backend
 from neon.initializers import Constant, GlorotUniform, Xavier
@@ -28,8 +28,43 @@ from neon.layers import Conv, Dropout, Pooling, GeneralizedCost, Affine
 from neon.optimizers import GradientDescentMomentum, Schedule, MultiOptimizer
 from neon.transforms import Rectlin, Softmax, CrossEntropyMulti, TopKMisclassification
 from neon.models import Model
-from neon.data import ImageLoader
 from neon.callbacks.callbacks import Callbacks
+
+from neon.data.dataloader_transformers import OneHot, TypeCast, BGRMeanSubtract
+from neon.data.aeon_shim import AeonDataLoader
+
+def wrap_dataloader(dl, dtype=np.float32):
+    dl = OneHot(dl, index=1, nclasses=1000)
+    dl = TypeCast(dl, index=0, dtype=dtype)
+    dl = BGRMeanSubtract(dl, index=0)
+    return dl
+
+def common_config(subset_pct, manifest_filename, manifest_root, batch_size, scale):
+    image_config = {"type": "image",
+                    "height": 224,
+                    "width": 224}
+    label_config = {"type": "label",
+                    "binary": False}
+    augmentation = {"type": "image",
+                    "scale": scale,
+                    "crop_enable": True}
+
+    return {'manifest_filename': manifest_filename,
+            'manifest_root': manifest_root,
+            'batch_size': batch_size,
+            'subset_fraction': float(subset_pct/100.0),
+            'etl': [image_config, label_config],
+            'augmentation': [augmentation]}
+
+def make_train_config(subset_pct, manifest_filename, manifest_root, batch_size, scale):
+    train_config = common_config(subset_pct, manifest_filename, manifest_root, batch_size, scale)
+    train_config['shuffle_enable'] = True
+    train_config['shuffle_manifest'] = True
+    return wrap_dataloader(AeonDataLoader(train_config))
+
+def make_val_config(subset_pct, manifest_filename, manifest_root, batch_size, scale):
+    val_config = common_config(subset_pct, manifest_filename, manifest_root, batch_size, scale)
+    return wrap_dataloader(AeonDataLoader(val_config))
 
 # parse the command line arguments
 parser = NeonArgparser(__doc__)
@@ -41,12 +76,18 @@ parser.add_argument('--test_only', action='store_true',
                     help='skip fitting - evaluate metrics on trained model weights')
 args = parser.parse_args()
 
-img_set_options = dict(repo_dir=args.data_dir, inner_size=224,
-                       subset_pct=args.subset_pct)
-train = ImageLoader(set_name='train', scale_range=(256, 384),
-                    shuffle=True, **img_set_options)
-test = ImageLoader(set_name='validation', scale_range=(256, 256), do_transforms=False,
-                   shuffle=False, **img_set_options)
+# Training set has different scale configuration (256, 384) than testing set (256, 256). Why?
+# Commenting it out gives the folowing runtime error:
+# RuntimeError: Unable to create internal loader object: value for 'scale' out of range
+#train = make_train_config(args.subset_pct, args.manifest["train"], args.manifest_root, batch_size=args.batch_size, scale=[0.875, 0.583])
+val = make_val_config(args.subset_pct, args.manifest["val"], args.manifest_root, batch_size=args.batch_size, scale=[0.875, 0.875])
+
+#img_set_options = dict(repo_dir=args.data_dir, inner_size=224,
+#                       subset_pct=args.subset_pct)
+#train = ImageLoader(set_name='train', scale_range=(256, 384),
+#                    shuffle=True, **img_set_options)
+#test = ImageLoader(set_name='validation', scale_range=(256, 256), do_transforms=False,
+#                   shuffle=False, **img_set_options)
 
 init1 = Xavier(local=True)
 initfc = GlorotUniform()
@@ -83,10 +124,10 @@ model = Model(layers=layers)
 
 # configure callbacks
 top5 = TopKMisclassification(k=5)
-callbacks = Callbacks(model, eval_set=test, metric=top5, **args.callback_args)
+callbacks = Callbacks(model, eval_set=val, metric=top5, **args.callback_args)
 
 model.load_params(args.model_file)
-mets=model.eval(test, metric=TopKMisclassification(k=5))
+mets=model.eval(val, metric=TopKMisclassification(k=5))
 print 'Validation set metrics:'
 print 'LogLoss: %.2f, Accuracy: %.1f %% (Top-1), %.1f %% (Top-5)' % (mets[0],
                                                                     (1.0-mets[1])*100,
