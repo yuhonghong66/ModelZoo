3e7736f673cbdea55ad2c72a494d8c0aaa9ca769
Tomasz Patejko <tomasz.patejko@intel.com>
2017-10-28 22:11:53 +0200

    Change in how output is stored in output.dat file

--- ImageClassification/CIFAR10/All_CNN/test.sh
+++ ImageClassification/CIFAR10/All_CNN/test.sh
@@ -24,7 +24,7 @@ echo "Downloading weights file from ${WEIGHTS_URL}"
 curl -o $WEIGHTS_FILE $WEIGHTS_URL 2> /dev/null
 
 python -u $TEST_SCRIPT -i ${EXECUTOR_NUMBER} -vvv \
-           --model_file $WEIGHTS_FILE --no_progress_bar | tee output.dat 2>&1
+           --model_file $WEIGHTS_FILE --no_progress_bar 2>&1 | tee output.dat
 rc=$?
 if [ $rc -ne 0 ];then
     exit $rc
